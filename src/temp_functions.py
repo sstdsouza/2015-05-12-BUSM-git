5ce1e7960d05e3df485aba98e547ad7b7d87c475
Stephanie D'Souza <sdsouza@bu.edu>
2015-05-13 10:25:30 -0400

    function to convert f to c

--- src/temp_functions.py
+++ src/temp_functions.py
@@ -1,6 +1,12 @@
-def k_to_c(temp)
+def k_to_c(temp):
 
 	return temp - 273.15
 
-def f_to_k(temp)
+def f_to_k(temp):
 	return ((temp -32)*(5/9)) + 273.15
+
+def f_to_c(temp):
+	temp_k = f_to_k(temp)
+	result = k_to_c(temp)
+	return result	
+
